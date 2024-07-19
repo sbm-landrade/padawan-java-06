fb6c03ea0d1b25440e905fb064c127c1cdbdc6ba
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-19 14:02:51 -0300

    DAO com inserção do produto

--- loja-virtual-repository/src/br/com/alura/jdbc/TestaPoolConexao.java
+++ loja-virtual-repository/src/br/com/alura/jdbc/TestaPoolConexao.java
@@ -2,6 +2,8 @@ package br.com.alura.jdbc;
 
 import java.sql.SQLException;
 
+import br.com.alura.jdbc.factory.ConnectionFactory;
+
 public class TestaPoolConexao {
 
 	public static void main(String[] args) throws SQLException {
