9054614fdecf758d096709452a5560404a5fe5a7
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-22 10:45:39 -0300

    Usando Inner Join

--- loja-virtual-repository/src/br/com/alura/jdbc/modelo/Categoria.java
+++ loja-virtual-repository/src/br/com/alura/jdbc/modelo/Categoria.java
@@ -1,10 +1,14 @@
 package br.com.alura.jdbc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
-	
+
 	private Integer id;
 	private String nome;
-	
+	private List<Produto> produtos = new ArrayList<Produto>();
+
 	public Categoria(Integer id, String nome) {
 		this.id = id;
 		this.nome = nome;
@@ -16,6 +20,14 @@ public class Categoria {
 
 	public int getId() {
 		return id;
+	}
+
+	public void adicionar(Produto produto) {
+		produtos.add(produto);
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
 	}
 
 }
