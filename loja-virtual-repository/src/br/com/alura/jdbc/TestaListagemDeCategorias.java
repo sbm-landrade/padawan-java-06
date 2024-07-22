3dfcbb1a789c21e17c007881055a9b9266c53ffb
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-22 10:13:26 -0300

    Queries N + 1

--- loja-virtual-repository/src/br/com/alura/jdbc/TestaListagemDeCategorias.java
+++ loja-virtual-repository/src/br/com/alura/jdbc/TestaListagemDeCategorias.java
@@ -5,19 +5,28 @@ import java.sql.SQLException;
 import java.util.List;
 
 import br.com.alura.jdbc.dao.CategoriaDAO;
+import br.com.alura.jdbc.dao.ProdutoDAO;
 import br.com.alura.jdbc.factory.ConnectionFactory;
 import br.com.alura.jdbc.modelo.Categoria;
+import br.com.alura.jdbc.modelo.Produto;
 
 public class TestaListagemDeCategorias {
 
 	public static void main(String[] args) throws SQLException {
 
-		try(Connection connection = new ConnectionFactory().recuperarConexao()) {
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
 			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
-
 			List<Categoria> listaDeCategorias = categoriaDAO.listar();
-			listaDeCategorias.stream().forEach(
-					ct -> System.out.println(ct.getNome()));
+			listaDeCategorias.stream().forEach(ct -> {
+				System.out.println(ct.getNome());
+				try {
+					for (Produto produto : new ProdutoDAO(connection).buscar(ct)) {
+						System.out.println(ct.getNome() + " - " + produto.getNome());
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			});
 		}
 	}
 }
