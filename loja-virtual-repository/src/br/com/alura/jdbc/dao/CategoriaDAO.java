fe2bfd75a1cb7f31cbd72ec60b94fbba84865993
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-22 13:34:14 -0300

    Apresentando a View de Produtos

--- loja-virtual-repository/src/br/com/alura/jdbc/dao/CategoriaDAO.java
+++ loja-virtual-repository/src/br/com/alura/jdbc/dao/CategoriaDAO.java
@@ -43,9 +43,8 @@ public class CategoriaDAO {
 		Categoria ultima = null;
 		List<Categoria> categorias = new ArrayList<>();
 
-		System.out.println("Executando a query de categoria");
-
-		String sql = "SELECT C.ID, C.NOME, P.ID, P.NOME, P.DESCRICAO FROM CATEGORIA C INNER JOIN"
+		String sql = "SELECT C.ID, C.NOME, P.ID, P.NOME, P.DESCRICAO" 
+				+ "FROM CATEGORIA C INNER JOIN"
 				+ " PRODUTO P ON C.ID = P.CATEGORIA_ID";
 
 		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
@@ -54,12 +53,14 @@ public class CategoriaDAO {
 			try (ResultSet rst = pstm.getResultSet()) {
 				while (rst.next()) {
 					if (ultima == null || !ultima.getNome().equals(rst.getString(2))) {
-						Categoria categoria = new Categoria(rst.getInt(1), rst.getString(2));
-						ultima = categoria;
+						Categoria categoria = 
+								new Categoria(rst.getInt(1), rst.getString(2));
+						
 						categorias.add(categoria);
+						ultima = categoria;
 					}
-					Produto produto
-					= new Produto(rst.getInt(3), rst.getString(4), rst.getString(5));
+					Produto produto = new Produto(rst.getInt(3), 
+							rst.getString(4), rst.getString(5));
 					ultima.adicionar(produto);
 				}
 			}
